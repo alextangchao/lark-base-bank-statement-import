0613aeaef0e242118bdad753108dd34ffda1ac74
alextangchao <tangzhou9@sina.cn>
2023-10-09 00:01:53 +0800

    add csv parsing, form structure, and header select

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import "./App.css";
-import { bitable, TableMeta } from "@lark-base-open/js-sdk";
-import { Button, Form } from "@douyinfe/semi-ui";
-import { BaseFormApi } from "@douyinfe/semi-foundation/lib/es/form/interface";
-import { useState, useEffect, useRef, useCallback } from "react";
+// @ts-ignore
+import {bitable, TableMeta} from "@lark-base-open/js-sdk";
+import {Button, Col, Form, Row, Select} from "@douyinfe/semi-ui";
+import {BaseFormApi} from "@douyinfe/semi-foundation/lib/es/form/interface";
+import {useState, useEffect, useRef, useCallback} from "react";
 import FileUpload from "./components/FileUpload";
+import {OptionProps} from "@douyinfe/semi-ui/lib/es/select";
 
 export default function App() {
     const [tableMetaList, setTableMetaList] = useState<TableMeta[]>();
+    const [billData, setBillData] = useState<string[][]>([[]]);
+    const [headerSelectOption, setHeaderSelectOption] = useState<OptionProps[]>([]);
+
     const formApi = useRef<BaseFormApi>();
+
+    // add record to table
     const addRecord = useCallback(
-        async ({ table: tableId }: { table: string }) => {
+        async ({table: tableId}: { table: string }) => {
             if (tableId) {
                 const table = await bitable.base.getTableById(tableId);
-                table.addRecord({
+                await table.addRecord({
                     fields: {},
                 });
             }
@@ -26,9 +33,19 @@ export default function App() {
             bitable.base.getSelection(),
         ]).then(([metaList, selection]) => {
             setTableMetaList(metaList);
-            formApi.current?.setValues({ table: selection.tableId });
+            formApi.current?.setValues({table: selection.tableId});
         });
     }, []);
+
+    // set header select option list
+    useEffect(() => {
+        console.log("update header select list");
+        setHeaderSelectOption(
+            billData.slice(0, 20).map((element: string[], index) => {
+                return {value: index, label: element.reduce((acc, curr) => acc + "-" + curr, "")};
+            })
+        );
+    }, [billData]);
 
     return (
         <main className="main">
@@ -42,52 +59,14 @@ export default function App() {
                     (formApi.current = baseFormApi)
                 }
             >
-                <Form.Slot label="Development guide">
-                    <div>
-                        <a
-                            href="https://lark-technologies.larksuite.com/docx/HvCbdSzXNowzMmxWgXsuB2Ngs7d"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Base Extensions Guide
-                        </a>
-                        、
-                        <a
-                            href="https://bytedance.feishu.cn/docx/HazFdSHH9ofRGKx8424cwzLlnZc"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            多维表格插件开发指南
-                        </a>
-                    </div>
-                </Form.Slot>
-                <Form.Slot label="API">
-                    <div>
-                        <a
-                            href="https://lark-technologies.larksuite.com/docx/Y6IcdywRXoTYSOxKwWvuLK09sFe"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Base Extensions Front-end API
-                        </a>
-                        、
-                        <a
-                            href="https://bytedance.feishu.cn/docx/HjCEd1sPzoVnxIxF3LrcKnepnUf"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            多维表格插件API
-                        </a>
-                    </div>
-                </Form.Slot>
                 <Form.Select
                     field="table"
                     label="Select Table"
                     placeholder="Please select a Table"
-                    style={{ width: "100%" }}
+                    style={{width: "100%"}}
                 >
                     {Array.isArray(tableMetaList) &&
-                        tableMetaList.map(({ name, id }) => {
+                        tableMetaList.map(({name, id}) => {
                             return (
                                 <Form.Select.Option key={id} value={id}>
                                     {name}
@@ -99,7 +78,64 @@ export default function App() {
                     Add Record
                 </Button>
             </Form>
-            <FileUpload />
+
+            <FileUpload data={billData} setData={setBillData}/>
+
+            <Form onValueChange={values => {
+            }}>
+                {
+                    ({formState, values, formApi}) => (
+                        <>
+                            <Row>
+                                <Col span={24}>
+                                    <Form.Select
+                                        field="header"
+                                        label="header"
+                                        placeholder="请选择业务线"
+                                        style={{width: "100%"}}
+                                        optionList={headerSelectOption}
+                                    ></Form.Select>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={12}>
+                                    <Form.Select field='business2' label="原字段" style={{width: '200px'}}>
+                                        <Form.Select.Option value="abc">Semi</Form.Select.Option>
+                                        <Form.Select.Option value="ulikeCam">轻颜相机</Form.Select.Option>
+                                        <Form.Select.Option value="toutiao">今日头条</Form.Select.Option>
+                                    </Form.Select>
+                                </Col>
+                                <Col span={12}>
+                                    <Form.Select field="role" label='角色' style={{width: '200px'}}>
+                                        <Form.Select.Option value="operate">运营</Form.Select.Option>
+                                        <Form.Select.Option value="rd">开发</Form.Select.Option>
+                                        <Form.Select.Option value="pm">产品</Form.Select.Option>
+                                        <Form.Select.Option value="ued">设计</Form.Select.Option>
+                                    </Form.Select>
+                                </Col>
+                                {Array.isArray(billData[formState.values.header]) &&
+                                    billData[formState.values.header].map((element: string, index) => {
+                                        const id = "aa" + index;
+                                        return (
+                                            <Col span={12}>
+                                                <Form.Select key={id} field={id} label={element} style={{width: "90%"}}>
+                                                    <Form.Select.Option value="operate">运营</Form.Select.Option>
+                                                    <Form.Select.Option value="rd">开发</Form.Select.Option>
+                                                    <Form.Select.Option value="pm">产品</Form.Select.Option>
+                                                    <Form.Select.Option value="ued">设计</Form.Select.Option>
+                                                </Form.Select>
+                                            </Col>
+                                        )
+                                    })
+                                }
+                            </Row>
+                            <code style={{marginTop: 24}}>{JSON.stringify(formState)}</code>
+                            {/*{console.log(billData[formState.values.header])}*/}
+                        </>
+                    )
+                }
+            </Form>
+            {/*<div>{JSON.stringify(billData)}</div>*/}
         </main>
     );
 }
--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
-import { Upload, Button } from "@douyinfe/semi-ui";
-import { customRequestArgs } from "@douyinfe/semi-ui/lib/es/upload/interface";
+import {useState} from "react";
+import {Upload, Button} from "@douyinfe/semi-ui";
+import {customRequestArgs} from "@douyinfe/semi-ui/lib/es/upload/interface";
+import Papa from "papaparse";
 
-export default function FileUpload() {
+// @ts-ignore
+export default function FileUpload({data, setData}) {
     let [fileName, setFileName] = useState("");
-    let [file, setFile] = useState<File>();
+    // let [data, setData] = useState({});
 
     let [showData, setShowData] = useState({});
 
-    function checkBill() {}
-
-    function openFile(requestArgs: customRequestArgs) {
-        requestArgs.onProgress({ total: 10, loaded: 5 });
-        setFileName(requestArgs.fileName);
-        setFile(requestArgs.fileInstance);
-
-        setTimeout(() => requestArgs.onSuccess({}), 1000);
-        console.log(requestArgs);
+    function checkBill() {
     }
+
+    async function openFile(requestArgs: customRequestArgs) {
+        requestArgs.onProgress({total: 10, loaded: 5});
+        setFileName(requestArgs.fileName);
+
+        // get csv data
+        // TODO: handle parsing error
+        Papa.parse(requestArgs.fileInstance, {
+            header: false,
+            skipEmptyLines: true,
+            error: (error: Object) => {
+                console.log(error)
+            },
+            complete: function (results: { data: any }) {
+                console.log(results.data);
+                setData(results.data)
+            },
+        });
+
+        requestArgs.onSuccess({});
+
+        // setTimeout(() => requestArgs.onSuccess({}), 1000);
+        // console.log(requestArgs);
+    }
+
+    // TODO: clear bill data when user remove file
 
     return (
         <>
@@ -32,13 +52,13 @@ export default function FileUpload() {
                 dragSubText="仅支持csv、xls、xlsx类型文件"
                 onSuccess={(data) => setShowData(data)}
                 customRequest={openFile}
-                style={{ marginTop: 10 }}
+                style={{marginTop: 10}}
             >
                 {/* <Button icon={<IconUpload />} theme="light">
                 点击上传
             </Button> */}
             </Upload>
-            <div>{JSON.stringify(showData)}</div>
+            {/* <div>{JSON.stringify(data)}</div> */}
         </>
     );
 }
